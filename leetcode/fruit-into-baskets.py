2f9c57b4de3d08931b639fb250ae4a0b70e465d0
Young19ermi <111576904+Young19ermi@users.noreply.github.com>
2024-01-11 05:14:27 -0800

    Added solution for Fruit Into Baskets

---
+++ leetcode/fruit-into-baskets.py
@@ -0,0 +1,53 @@
+# from collections import defaultdict
+
+# class Solution:
+#     def totalFruit(self, nums: List[int]) -> int:
+#         count = 0
+#         maximum = 0
+#         i = 0
+#         countme = defaultdict(int)
+#         for j in range(len(nums)):
+#             countme[nums[j]] = 1 + countme.get(nums[j], 0)
+#             while len(countme.keys()) > 2 and i < len(nums):
+#                 countme[nums[i]] -= 1
+#                 if countme[nums[i]] == 0:
+#                     del countme[nums[i]]
+#                 count -= 1
+#                 i += 1
+#             count += 1
+#             maximum = max(count, maximum)
+#         return maximum
+
+class Solution:
+    def totalFruit(self, nums: List[int]) -> int:
+        count = 0
+        maximum = 0
+        i = 0
+        countme = defaultdict(int)
+        for j in range(len(nums)):
+            countme[nums[j]] = 1 + countme.get(nums[j], 0)
+            while len(countme.keys()) > 2 and i < len(nums):
+                countme[nums[i]] -= 1
+                if countme[nums[i]] == 0:
+                    del countme[nums[i]]
+                i += 1
+                count -= 1
+            count += 1
+            maximum = max(count, maximum)
+        return maximum
+
+
+
+# for j in range(1,len(nums)):
+        #     if nums[j] != nums[i]:
+        #         offset += 1
+        #         if offset >= 2:
+        #             i += 1
+        #             count -= 1
+        #             maximum = max(maximum, count)
+        #     else:
+        #         count += 1
+                
+        # return maximum
+
+
